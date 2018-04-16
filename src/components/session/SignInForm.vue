8d34d26d59265dd428a6373d48ff998a349c018c
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2018-04-16 19:51:29 +0300

    Added SignIn form and session tabs

---
+++ src/components/session/SignInForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-form v-model="valid" class="signInForm" @submit="signIn()">
+    <v-text-field
+      label="E-mail"
+      v-model="email"
+      :rules="emailRules"
+      required
+    ></v-text-field>
+    <v-text-field
+      label="Password"
+      v-model="password"
+      type="password"
+      :rules="passwordRules"
+      :counter="10"
+      required
+    ></v-text-field>
+    <v-btn 
+      depressed small color="primary"
+      class="mt-4"
+      type="submit">Ввійти</v-btn>
+    </v-form>
+</template>
+
+<script>
+  export default {
+    name: 'SignInForm',
+    data: () => ({
+      valid: false,
+      password: '',
+      passwordRules: [
+        v => !!v || 'password is required',
+        v => v.length >= 10 || 'Password must be at least 10 characters'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ]
+    }),
+    methods: {
+      signIn () {
+        console.log(this.password, this.email)
+      }
+    }
+  }
+</script>
+<style scoped lang='sass'>
+.signInForm
+  padding: 20px
+  text-align: right
+</style>
--- src/screens/session/SessionView.vue
+++ src/screens/session/SessionView.vue
@@ -1,33 +1,57 @@
 <template>
-  <div class="sesion_view">
+  <div class="session_view">
       <v-tabs
         centered
+        v-model="active"
         color="white"
         slot="extension"
         :fixed-tabs="false"
-        slider-color="blue">
-        <v-tab :key="`SignUp`" :href="`#tab-SignUp`">
-          Sign Up
+        slider-color="blue"
+        class="session_tabs elevation-2"
+        >
+        <v-tab key="SignUp">
+          <h4>Ввійти</h4>
         </v-tab>
-        <v-tab :key="`SignIn`" :href="`#tab-SignIn`">
-          Sign In
+        <v-tab key="SignIn">
+          <h4>Реєстрація</h4>
         </v-tab>
+        <v-tabs-items v-model="active">
+          <v-tab-item key="SignUp">
+            <v-card flat>
+                <sign-in-form></sign-in-form>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item key="SignIn">
+            <v-card flat>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
       </v-tabs>
-    <v-tabs-items>
-      <v-tab-item :key="`SignUp`" :id="`tab-signUp`">
-      </v-tab-item>
-      <v-tab-item :key="`SignIn`" :id="`tab-signIn`">
-      </v-tab-item>
-    </v-tabs-items>
   </div>
 </template>
-
 <script>
+import SignInForm from '@/components/session/SignInForm'
+
 export default {
+  data () {
+    return {
+      active: null
+    }
+  },
+  components: {
+    SignInForm
+  }
 }
 </script>
 
 <style scoped lang="sass">
-session_view
-  color: green
+.session_view
+  display: flex
+  align-items: center
+  justify-content: center
+  min-height: 100%
+  .session_tabs
+    trasnsition: all .2s ease
+    width: 400px
+    min-height: 250px
 </style>
