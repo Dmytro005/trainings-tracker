eab5406c3793b4f5b1b8365aa76dd05d83c086c9
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2018-04-16 22:07:43 +0300

    Added firebase and VueFire

--- src/main.js
+++ src/main.js
@@ -3,20 +3,34 @@
 import Vue from 'vue'
 import App from './App'
 import Vuetify from 'vuetify'
+import firebase from 'firebase'
+import VueFire from 'vuefire'
 import 'vuetify/dist/vuetify.min.css'
-
 import './theme/main.sass'
 
 import router from './router'
 import store from './store'
 
+const firebaseApp = firebase.initializeApp({
+  apiKey: 'AIzaSyCQPaJwAnY_CFgGXdWg0R6MitqUbGYVbqQ',
+  authDomain: 'trainings-tracker.firebaseapp.com',
+  databaseURL: 'https://trainings-tracker.firebaseio.com',
+  projectId: 'trainings-tracker',
+  storageBucket: 'trainings-tracker.appspot.com',
+  messagingSenderId: '671859954787'
+})
+
+// const db = firebaseApp.database()
+
 Vue.use(Vuetify)
+Vue.use(VueFire)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  firebase: firebaseApp,
   store,
   router,
   components: { App },
